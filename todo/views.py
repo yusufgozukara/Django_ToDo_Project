697f14c506998ef6683f954e59dca5767305fe66
yusufgozukara <yusufgozukara11@gmail.com>
2022-09-01 11:32:45 +0300

    home template added

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Welcome Body')
+    todos = Todo.objects.all()
+    context = {
+        'todos' : todos
+    }
+
+    return render(request, 'todo/home.html', context)
