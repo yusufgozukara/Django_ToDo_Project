770040b161f8da037d4a1fc543834fb9d27e367e
yusufgozukara <yusufgozukara11@gmail.com>
2022-09-01 12:58:17 +0300

    home html extended

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
+
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
 from .forms import TodoForm
@@ -8,14 +9,29 @@ from .forms import TodoForm
 
 def home(request):
     todos = Todo.objects.all()
+    form = TodoForm
+    # if request.method == 'POST':
+    #     form = TodoForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('home')
+    #submit yapınca bilgiyi göndermesi için ya create'deki bu metodu buraya taşımak 
+    #ya da kopyalamak gerekir. ya da formun action kısmına /add/ yazıp oraya istek gönderebiliriz. 
+
     context = {
-        'todos' : todos
+        'todos' : todos,
+        'form' : form
     }
 
     return render(request, 'todo/home.html', context)
 
 def todo_create(request):
     form = TodoForm()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
     context = {
         "form" : form
